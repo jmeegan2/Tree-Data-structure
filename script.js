fb37a16864dad803dbaa74c337897fbdf0dc4ba0
James Meegan <jmeegan2@owlmail.harford.edu>
2022-01-06 23:20:08 -0500

    done

--- script.js
+++ script.js
@@ -21,9 +21,52 @@ class Tree {
 
         if(parent) 
             parent.children.push(node)
+        else
+            if(!this.root)
+                this.root = node;
+            else 
+                return "Tried to store node at root when root already exist"
+    }
+
+    findBFS(data) {
+        const queue = [this.root];
+        let _node = null;
+
+        this.traverseBFS((node) => {
+            if(node.data == data)
+                _node = node;
+        })
+
+        return _node;
+    }
+
+    traverseBFS(cb) { // cb means call back
+        const queue = [this.root]
+
+        if(cb)
+            while(queue.length) {
+                const node = queue.shift();
+
+                cb(node)
+
+                for(const child of node.children)
+                    queue.push(child)
+            }
     }
 }
 
 (function test() {
-    console.log('test')
+    let tree = new Tree();
+
+
+    tree.add("Node1")
+    tree.add("Node2", "Node1")
+    tree.add("Node3", "Node1")
+    tree.add("Node4", "Node2")
+    tree.add("Node5", "Node2")
+    tree.add("Node6", "Node3")
+
+    tree.traverseBFS((node) => {console.log("Current node: ", node)})
 })()
+
+//used this video: https://www.youtube.com/watch?v=K7VnBuOlCI8
